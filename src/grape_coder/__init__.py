f14b5ede313435f28c123bf0d4ae7b1ff1e0b637
Luca Mandrelli <luca.mandrelli@icloud.com>
2025-11-09 21:43:23 +0100

    feat(setup): basic typer app

--- src/grape_coder/__init__.py
+++ src/grape_coder/__init__.py
@@ -1,2 +1,1 @@
-def main() -> None:
-    print("Hello from grape-coder!")
+"""Grape Coder CLI package."""
---
+++ src/grape_coder/main.py
@@ -0,0 +1,28 @@
+import typer
+import importlib.metadata
+from typing import Optional
+
+app = typer.Typer(no_args_is_help=True)
+
+
+def version_callback(value: bool):
+    if value:
+        version = importlib.metadata.version("grape-coder")
+        typer.echo(version)
+        raise typer.Exit()
+
+
+@app.command()
+def main(
+    arg: str = typer.Argument(..., help="Argument to greet"),
+    version: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        "-v",
+        callback=version_callback,
+        is_eager=True,
+        help="Show version and exit",
+    ),
+):
+    """Hello {arg} ! This is grape-coder."""
+    typer.echo(f"Hello {arg} ! This is grape-coder.")
