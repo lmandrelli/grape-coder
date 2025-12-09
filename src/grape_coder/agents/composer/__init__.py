c528c546e1e53627d185a78f998370d2e33190ac
rom100main <rom100main@proton.me>
2025-12-09 14:35:34 +0100

    feat: visibility functions

--- src/grape_coder/agents/composer/__init__.py
+++ src/grape_coder/agents/composer/__init__.py
@@ -1,11 +1,5 @@
 from .composer import build_composer
-from .generate_class import create_class_agent
-from .orchestrator import create_orchestrator_agent
-from .text import create_text_agent
 
 __all__ = [
-    "create_class_agent",
     "build_composer",
-    "create_orchestrator_agent",
-    "create_text_agent",
 ]
